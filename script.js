c3ca2efc43e0985a84226683c7ed890fc388fe59
Matthew Collins <matthewlcollins12@gmail.com>
2025-02-21 18:46:39 +1100

    Finished first parts, everything working by looks

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const container = document.querySelector(".container")
 const button = document.querySelector(".button")
+const slideRange = document.getElementById("myRange")
 
 function onStartUp() {
+    document.getElementById("myRange").value = 16
     for (i = 0; i < (16*16); i++) {
         const box = document.createElement("div")
         box.classList.add("box")
@@ -15,6 +17,11 @@ function onStartUp() {
 
 onStartUp();
 
+function clearBoxes() {
+    while (container.firstChild) {
+        container.firstChild.remove()
+    }
+}
 
 button.addEventListener("click", () => {
     const divChildren = document.querySelectorAll(".box")
@@ -22,3 +29,21 @@ button.addEventListener("click", () => {
         divChildren[i].style.backgroundColor = "white"
     }
 })
+
+function slider() {
+    clearBoxes()
+    const multiNum = document.getElementById("myRange").value
+    document.getElementById("boxnum").textContent = multiNum + " x " + multiNum + " grid"
+    for (i = 0; i < (multiNum*multiNum); i++) {
+        const box = document.createElement("div")
+        box.classList.add("box")
+        box.style.cssText = `border: 2px solid; background-color: white; height: ${(800/multiNum)}px; aspect-ratio: 1`
+        box.addEventListener("mouseover", () => {
+            box.style.backgroundColor = "black"
+        })
+        container.appendChild(box)
+    }
+}
+
+slideRange.addEventListener("input", slider)
+
